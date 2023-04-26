810c7c20aff0b9755ee3b0add46e86bd5cdbfeac
Heba-Alii <hebaaliali18@gmail.com>
2023-04-26 20:39:46 +0200

    create new user with mail , pass and connect with firebase Auth

---
+++ app/src/main/java/com/example/developers/view/main/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.developers.view.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.developers.R;
+import com.example.developers.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        navController.navigate(R.id.registerFragment);
+
+    }
+}
---
+++ app/src/main/java/com/example/developers/view/registration/RegisterFragment.java
@@ -0,0 +1,70 @@
+package com.example.developers.view.registration;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.developers.R;
+import com.example.developers.databinding.FragmentRegisterBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterFragment extends Fragment {
+    FragmentRegisterBinding binding;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View registerFragment = inflater.inflate(R.layout.fragment_register, container, false);
+        binding = FragmentRegisterBinding.bind(registerFragment);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail = binding.mail.getText().toString();
+                String pass = binding.pass.getText().toString();
+                if (datavalid(mail, pass)) {
+                    addToFireBase(mail, pass);
+                } else {
+                    Toast.makeText(getActivity(), "Please Complete your data", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void addToFireBase(String mail, String pass) {
+        firebaseAuth.createUserWithEmailAndPassword(mail, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Navigation.findNavController(binding.getRoot()).navigate(R.id.action_registerFragment_to_homeFragment);
+                    // getActivity().finish();
+                } else {
+                    Log.e("TAG", "onComplete: " + task.getException().getMessage());
+                    Toast.makeText(getActivity(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean datavalid(String mail, String pass) {
+        return !mail.isEmpty() && !pass.isEmpty();
+    }
+}
