e2f7a1505ebcb030c405c27decaf6cd07e02b457
Heba-Alii <hebaaliali18@gmail.com>
2023-05-02 16:29:07 +0300

    if user login the app will open on Home Fragment

--- app/src/main/java/com/example/developers/view/main/MainActivity.java
+++ app/src/main/java/com/example/developers/view/main/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.developers.view.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.developers.R;
 import com.example.developers.databinding.ActivityMainBinding;
+import com.example.developers.databinding.FragmentHomeBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +24,10 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
-        NavController navController = navHostFragment.getNavController();
-        navController.navigate(R.id.registerFragment);
-
+        if (firebaseAuth.getCurrentUser() == null) {
+            NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+            NavController navController = navHostFragment.getNavController();
+           navController.navigate(R.id.registerFragment);
+        }
     }
 }
