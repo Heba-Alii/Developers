e9446a9ff09c0d50d1dcec20eb4cd320b5ff697f
Heba-Alii <hebaaliali18@gmail.com>
2023-05-12 01:57:52 +0300

    add Entity

---
+++ app/src/main/java/com/example/developers/model/pojo/DeveloperEntity.java
@@ -0,0 +1,102 @@
+package com.example.developers.model.pojo;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "dev_table")
+public class DeveloperEntity {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String name;
+    private String title;
+    private double salary;
+    private double ponus;
+    private int absence;
+    private int vacation;
+    private int medicalInsurance;
+    private int socialInsurance;
+
+    public DeveloperEntity(int id, String name, String title, double salary, double ponus, int absence, int vacation, int medicalInsurance, int socialInsurance) {
+        this.id = id;
+        this.name = name;
+        this.title = title;
+        this.salary = salary;
+        this.ponus = ponus;
+        this.absence = absence;
+        this.vacation = vacation;
+        this.medicalInsurance = medicalInsurance;
+        this.socialInsurance = socialInsurance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getPonus() {
+        return ponus;
+    }
+
+    public void setPonus(double ponus) {
+        this.ponus = ponus;
+    }
+
+    public int getAbsence() {
+        return absence;
+    }
+
+    public void setAbsence(int absence) {
+        this.absence = absence;
+    }
+
+    public int getVacation() {
+        return vacation;
+    }
+
+    public void setVacation(int vacation) {
+        this.vacation = vacation;
+    }
+
+    public int getMedicalInsurance() {
+        return medicalInsurance;
+    }
+
+    public void setMedicalInsurance(int medicalInsurance) {
+        this.medicalInsurance = medicalInsurance;
+    }
+
+    public int getSocialInsurance() {
+        return socialInsurance;
+    }
+
+    public void setSocialInsurance(int socialInsurance) {
+        this.socialInsurance = socialInsurance;
+    }
+}
--- app/src/main/java/com/example/developers/view/developers/DeveloperAdapter.java
+++ app/src/main/java/com/example/developers/view/developers/DeveloperAdapter.java
@@ -1,16 +1,22 @@
 package com.example.developers.view.developers;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.developers.R;
 
 public class DeveloperAdapter extends RecyclerView.Adapter<DeveloperAdapter.ViewHolder> {
     @NonNull
     @Override
     public DeveloperAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.developer_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
@@ -24,8 +30,17 @@ public class DeveloperAdapter extends RecyclerView.Adapter<DeveloperAdapter.View
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView dev_img;
+        TextView dev_name;
+        TextView dev_title;
+        TextView dev_salary;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            dev_img = itemView.findViewById(R.id.dev_img);
+            dev_name = itemView.findViewById(R.id.dev_name);
+            dev_title = itemView.findViewById(R.id.dev_title);
+            dev_salary = itemView.findViewById(R.id.dev_salary);
         }
     }
 }
